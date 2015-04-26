80c44771d279a0f5c4572bd25cfb3066b808ab17
Naoki Ishikawa <naokiishikawa0808@gmail.com>
2015-04-26 16:05:27 +0900

    Added merge sort (with list).

--- c_algorithm_and_data_structure/sort.c
+++ c_algorithm_and_data_structure/sort.c
@@ -165,5 +165,82 @@ void merge_sort_array(int a[], int low, int high) {
   }
 }
 
+//連結リストのセル
+struct node {
+  int value; //キーとなる値
+  struct node *next; //次のセルへのポインタ
+};
+
+//2つの連結リストa, bをマージする マージされた連結リストの先頭要素へのポインタを返す
+struct node *merge_list(struct node *a, struct node *b) {
+  struct node head, *p;
+
+  //ポインタpがダミーの要素を指すようにする
+  p = &head;
+
+  //連結リストa, bのいずれかが空になるまで繰り返す
+  while (a != NULL && b != NULL) {
+    //連結リストa, bの先頭の要素を比較する
+    if (a->value <= b->value) {
+      //連結リストaの先頭の要素を取り除き、マージ済み連結リストの末尾に連結する
+      p->next = a;
+      p = a;
+      a = a->next;
+    } else {
+      //連結リストbの先頭の要素を取り除き、マージ済み連結リストの末尾に連結する
+      p->next = b;
+      p = b;
+      b = b->next;
+    }
+  }
+
+  //残っている要素をマージ済み連結リストの最後尾に連結する
+  if (a == NULL) {
+    p->next = b;
+  } else {
+    p->next = a;
+  }
+
+  return head.next;
+}
+
+//連結リスト版マージソート
+struct node *merge_sort_list(struct node *x) {
+  struct node *a, *b, *p;
+
+  //連結リストに要素が無いか、1つしか無いときはそのまま返す
+  if (x == NULL || x->next == NULL) {
+    return x;
+  }
+
+  //連結リストをスキャンするポインタ初期化
+
+  //aは1番目の要素を示す
+  a = x;
+
+  //bは3番目の要素（もし連結リストの長さが2なら2)を指す
+  b = x->next;
+  if (b != NULL) {
+    b = b->next;
+  }
+
+  //ポインタbが連結リストの末尾に到達するまでポインタaを1つ進め、ポインタbを2つ進める
+  //ポインタbが末尾に到達した時、ポインタaは連結リストのほぼ中央の要素をさしているはずである
+  while (b != NULL) {
+    a = a->next;
+    b = b->next;
+    if (b != NULL) {
+      b = b->next;
+    }
+  }
+
+  //連結リストをポインタaが指す要素の直後で2つに切断する
+  p = a->next;
+  a->next = NULL;
+
+  //切断した連結リストを個別に整列して、その結果をマージする
+  return merge_list(merge_sort_list(x), merge_sort_list(p));
+}
+
 int main () {
 }
